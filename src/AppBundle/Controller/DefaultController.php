eca0d5cdbfc16fe0c5e656a149a3d84f092538e9
Ellena Parsons <ellena.parsons@warrant-group.com>
2017-04-19 14:55:32 +0100

    added randomVariable to DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+
+        $randomVariable = true;
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
